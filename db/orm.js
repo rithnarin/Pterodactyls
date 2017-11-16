c807b88eec0fac79373f32a7a9aef33771874529
Rithnarin Kong <rithnarin@yahoo.com>
2017-11-15 18:12:47 -0800

    Finish tables for sequelize, may need more work

---
+++ db/orm.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+let db = new Sequelize();
+
+const Posts = db.define('Posts', {
+  id: Sequelize.INTEGER,
+  id_users: Sequelize.INTEGER,
+  title: Sequelize.STRING,
+  subtitle: Sequelize.STRING,
+  pics: Sequelize.STRING,
+  id_mongo_text: Sequelize.INTEGER,
+  id_locations: Sequelize.INTEGER,
+});
+
+const Locations = db.define('Locations', {
+  id: Sequelize.INTEGER,
+  location: Sequelize.STRING
+});
+
+const Users = db.define('Users', {
+  id: Sequelize.INTEGER,
+  username: Sequelize.STRING,
+  email: Sequelize.STRING,
+  about_me: Sequelize.STRING,
+  pic: Sequelize.STRING
+});
+
+const Sessions = db.define('Sessions', {
+  id: Sequelize.INTEGER,
+  id_users: Sequelize.INTEGER,
+  hash: Sequelize.STRING
+});
