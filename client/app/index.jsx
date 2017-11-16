2aa3207be3c9603349fd9215c0de3f84012ed722
Rithnarin Kong <rithnarin@yahoo.com>
2017-11-15 19:05:25 -0800

    Finish Search

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import {render} from 'react-dom';
+import axios from 'axios';
+import NavBar from 'navbar';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postsSearch: [];
+    };
+    this.search = this.search.bind(this);
+  }
+
+  search(query) {
+    axios.get('/search', {
+      params: {
+        search: query
+      }
+    })
+    .then((response) => this.setState({ postsSearch: response }))
+    .then(() => console.log('Searched!'));
+  }
+
   render () {
-    return <p> Hello React!</p>;
+    <div>
+      <NavBar search={this.search} />
+    </div>
   }
 }
 
