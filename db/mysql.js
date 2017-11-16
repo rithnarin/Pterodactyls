efeb92bf500a130c1da2af375091b61df1b120a3
Hack Reactor Student <student@hackreactor.com>
2017-11-16 12:32:56 -0800

    Improve fake data with faker, connect to mongo

--- db/mysql.js
+++ db/mysql.js
@@ -1,4 +1,6 @@
+var mongo = require('./mongo.js');
 var mysql = require('mysql2');
+var faker = require('faker');
 
 var connection = mysql.createConnection({
   host: process.env.SQL_HOST,
@@ -9,66 +11,16 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-// generate random data for the database
-// todo: instead use npm grunt-ipsum!!!
 
-const makeRandomString = function(n) {
-  var text = '';
-  var possible = 'abcdefghijklmnopqrstuvwxyz';
-  for (var i = 0; i < n; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-
-const makeRandomTitle = function() {
-  var text = '';
-  for (var i = 0; i < 6; i++) {
-    text += makeRandomString(Math.floor(Math.random() * 8)) + ' ';
-  }
-  return text;
-};
-
-const makeRandomSubtitle = function() {
-  var text = '';
-  for (var i = 0; i < 12; i++) {
-    text += makeRandomString(Math.floor(Math.random() * 8)) + ' ';
-  }
-  return text;
-};
-
-const makeRandomText = function(n) {
-  var text = '';
-  for (var i = 0; i < n; i++) {
-    text += makeRandomString(Math.floor(Math.random() * 8)) + ' ';
-  }
-  return text;
-};
-
-const sampleLocations = ['Boston', 'Thailand', 'Hawaii', 'Costa Rica', 'Morocco', 'Zanzibar'];
-const getRandomLocation = function() {
-  return sampleLocations[Math.floor(Math.random() * sampleLocations.length)];
-};
-
-
-
-// populate locations table
-for (var i = 0; i < sampleLocations.length; i++) {
-  let locationEntry = { location: sampleLocations[i] };
-  connection.query('INSERT INTO Locations SET ?', locationEntry,
-    (err, results, fields) => {
-      if (err) { console.log(err); }
-    }
-  );
-}
+/* generate data for the sql database user faker */
 
 // populate users table
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 30; i++) {
   let userEntry = { 
-    username: makeRandomString(7),
-    // password: makeRandomString(13),
-    email: makeRandomString(8) + '@' + makeRandomString(6) + '.com',
-    about_me: makeRandomText(30)
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    about_me: faker.lorem.paragraph(),
+    pic: faker.image.avatar()
   };
   connection.query('INSERT INTO Users SET ?', userEntry,
     (err, results, fields) => {
@@ -77,18 +29,39 @@ for (var i = 0; i < 10; i++) {
   );
 }
 
-// populate posts table
-for (var i = 0; i < 10; i++) {
-  let postsEntry = { 
-    id_users: Math.floor(Math.random() * 10),
-    title: makeRandomTitle(),
-    subtitle: makeRandomSubtitle(),
-    id_mongo_text: Math.floor(Math.random() * 10),
-    id_locations: Math.floor(Math.random() * sampleLocations.length)
+// populate locations table
+for (var i = 0; i < 30; i++) {
+  let locationEntry = { 
+    location: faker.address.city() + ' ' + faker.address.country() 
   };
-  connection.query('INSERT INTO Posts SET ?', postsEntry,
+  connection.query('INSERT INTO Locations SET ?', locationEntry,
     (err, results, fields) => {
       if (err) { console.log(err); }
     }
   );
 }
+
+// populate posts table
+
+mongo.Post.find({}, (err, mongoPosts) => {
+
+  // console.log(mongoPosts.length);
+
+  for (var i = 0; i < 10; i++) {
+    let mongoId = mongoPosts[i]["_id"];
+    let postsEntry = { 
+      id_users: Math.floor(Math.random() * 30),
+      title: faker.lorem.words(6),
+      subtitle: faker.lorem.sentence(),
+      id_mongo_text: mongoId,
+      id_locations: Math.floor(Math.random() * 30)
+    };
+    connection.query('INSERT INTO Posts SET ?', postsEntry,
+      (err, results, fields) => {
+        if (err) { console.log(err); }
+      }
+    );
+  }
+
+});
+
