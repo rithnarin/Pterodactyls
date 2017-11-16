a43b1506bea39d94cd79449ae9aa45fef3816036
Hack Reactor Student <student@hackreactor.com>
2017-11-15 20:53:54 -0800

    Refactored to class component, fixed typos, added onChange handler

--- client/app/components/navbar.jsx
+++ client/app/components/navbar.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 
-const NavBar = (props) => (
-  <nav>
-    <a href="/">KUYiK</a>
-    //onKeyPress should invoke search function
-    <input onKeyPress={(e) => if (e.keyCode === 13) props.search(e.target.value)} value="Search"></input>
-    <a href="#">Sign In</a>
-    <a href="#">Sign Up</a>
-  </nav>
-);
+//onKeyPress should invoke search function
+
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ''
+    },
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }  
+  
+  handleKeyUp(e) {
+    if (e.keyCode === 13) { 
+      this.props.search(this.state.searchText); 
+    }
+  }
+  
+  handleChange(e) {
+    this.setState({
+      searchText: e.target.value
+    });
+  }
+  
+  render() {
+    return (<nav>
+      <a href="/">KUYiK</a>
+      <input onKeyUp={this.handleKeyUp}
+        onChange={this.handleChange}
+        value="Search">
+      </input>
+      <a href="#">Sign In </a>
+      <a href="#">Sign Up</a>
+    </nav>);
+  }
+  
+}
+
+export default NavBar;
