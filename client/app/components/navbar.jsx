a5d37e08ed1c1fd56e2d2eae5922687c876fc52e
Rithnarin Kong <rithnarin@yahoo.com>
2017-11-15 18:15:40 -0800

    add sign in and sign up to navbar

--- client/app/components/navbar.jsx
+++ client/app/components/navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const NavBar = (props) => (
   <nav>
-    <a href="#">KUYiK</a>
-    <input value="Search"></input>
+    <a href="/">KUYiK</a>
+    //onKeyPress should invoke search function
+    <input onKeyPress={(e) => if (e.keyCode === 13) props.search(e.target.value)} value="Search"></input>
+    <a href="#">Sign In</a>
+    <a href="#">Sign Up</a>
   </nav>
 );
