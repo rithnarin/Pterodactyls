8815600a22cfa6b216e6e0cc670411356e3c910f
Hack Reactor Student <student@hackreactor.com>
2017-11-16 17:00:37 -0800

    Comment out schema, as now using sequelize

--- db/schema.sql
+++ db/schema.sql
@@ -10,81 +10,81 @@ CREATE DATABASE kuyikSQL;
 
 USE kuyikSQL;
 
--- ---
--- Globals
--- ---
+-- -- ---
+-- -- Globals
+-- -- ---
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
+-- -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- -- SET FOREIGN_KEY_CHECKS=0;
 
--- ---
--- Table 'Posts'
--- 
--- ---
+-- -- ---
+-- -- Table 'Posts'
+-- -- 
+-- -- ---
 
-DROP TABLE IF EXISTS `Posts`;
+-- DROP TABLE IF EXISTS `Posts`;
     
-CREATE TABLE `Posts` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `id_users` INTEGER NULL,
-  `title` VARCHAR(255) NOT NULL DEFAULT '''',
-  `subtitle` VARCHAR(255) NOT NULL DEFAULT '''',
-  `pics` VARCHAR(255) NULL,
-  `id_mongo_text` VARCHAR(255) NULL UNIQUE DEFAULT NULL,
-  `id_locations` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `Posts` (
+--   `id` INTEGER NOT NULL AUTO_INCREMENT,
+--   `id_users` INTEGER NULL,
+--   `title` VARCHAR(255) NOT NULL DEFAULT '''',
+--   `subtitle` VARCHAR(255) NOT NULL DEFAULT '''',
+--   `pics` VARCHAR(255) NULL,
+--   `id_mongo_text` VARCHAR(255) NULL UNIQUE DEFAULT NULL,
+--   `id_locations` INTEGER NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
 
--- ---
--- Table 'Locations'
--- 
--- ---
+-- -- ---
+-- -- Table 'Locations'
+-- -- 
+-- -- ---
 
-DROP TABLE IF EXISTS `Locations`;
+-- DROP TABLE IF EXISTS `Locations`;
     
-CREATE TABLE `Locations` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `location` VARCHAR(255) NULL UNIQUE DEFAULT '''',
-  PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `Locations` (
+--   `id` INTEGER NOT NULL AUTO_INCREMENT,
+--   `location` VARCHAR(255) NULL UNIQUE DEFAULT '''',
+--   PRIMARY KEY (`id`)
+-- );
 
--- ---
--- Table 'Users'
--- 
--- ---
+-- -- ---
+-- -- Table 'Users'
+-- -- 
+-- -- ---
 
-DROP TABLE IF EXISTS `Users`;
+-- DROP TABLE IF EXISTS `Users`;
     
-CREATE TABLE `Users` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL UNIQUE DEFAULT '''',
-  `email` VARCHAR(255) NULL UNIQUE DEFAULT NULL,
-  `about_me` VARCHAR(510) NULL DEFAULT NULL,
-  `pic` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `Users` (
+--   `id` INTEGER NOT NULL AUTO_INCREMENT,
+--   `username` VARCHAR(255) NOT NULL UNIQUE DEFAULT '''',
+--   `email` VARCHAR(255) NULL UNIQUE DEFAULT NULL,
+--   `about_me` VARCHAR(510) NULL DEFAULT NULL,
+--   `pic` VARCHAR(255) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
 
--- ---
--- Table 'Sessions'
--- 
--- ---
+-- -- ---
+-- -- Table 'Sessions'
+-- -- 
+-- -- ---
 
-DROP TABLE IF EXISTS `Sessions`;
+-- DROP TABLE IF EXISTS `Sessions`;
     
-CREATE TABLE `Sessions` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `id_users` INTEGER NOT NULL,
-  `hash` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `Sessions` (
+--   `id` INTEGER NOT NULL AUTO_INCREMENT,
+--   `id_users` INTEGER NOT NULL,
+--   `hash` VARCHAR(255) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
 
--- ---
--- Foreign Keys 
--- ---
+-- -- ---
+-- -- Foreign Keys 
+-- -- ---
 
-ALTER TABLE `Posts` ADD FOREIGN KEY (id_users) REFERENCES `Users` (`id`);
-ALTER TABLE `Posts` ADD FOREIGN KEY (id_locations) REFERENCES `Locations` (`id`);
-ALTER TABLE `Sessions` ADD FOREIGN KEY (id_users) REFERENCES `Users` (`id`);
+-- ALTER TABLE `Posts` ADD FOREIGN KEY (id_users) REFERENCES `Users` (`id`);
+-- ALTER TABLE `Posts` ADD FOREIGN KEY (id_locations) REFERENCES `Locations` (`id`);
+-- ALTER TABLE `Sessions` ADD FOREIGN KEY (id_users) REFERENCES `Users` (`id`);
 
 -- ---
 -- Table Properties
