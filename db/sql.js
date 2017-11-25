ade31d5dd7ef27b9323084a1827680f08708b1c9
Ephraim Glick <ephraim.glick@gmail.com>
2017-11-25 01:51:02 -0800

    Add file to automatically create database
    We no longer need to load the sql schema on the command line

---
+++ db/sql.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Promise = require('bluebird');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  user: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD
+});
+
+const db = Promise.promisifyAll(connection);
+
+connection.connectAsync()
+  .then(() => console.log('mySQL is now connected'))
+  .then(() => db.queryAsync('CREATE DATABASE kuyikSQL'))
+  .catch(err => {}) // ignore error caused when db already exists
+  .then(() => db.queryAsync('USE kuyikSQL'))
+  .then(() => console.log('mySQL is using the kuyikSQL database'));
