41afa6375380e8f2914d69ec940d9c093e5a1389
Ephraim Glick <ephraim.glick@gmail.com>
2017-11-26 20:14:14 -0800

    Add authentication with passport, express-sessions, and Google oAuth2

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const db = require('../db/orm.js');
 const mongo = require('../db/mongo.js');
 
 // comment out if db already populated
-require('../db/saveFakeData.js');
+// require('../db/saveFakeData.js');
 
 let app = express();
 
@@ -14,8 +14,69 @@ app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+/////////// for auth /////////////
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+app.use(session({ 
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  resave: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// these two are needed for sessions (using default storage)
+passport.serializeUser(function(user, done) {
+  done(null, user.google_id); 
+});
+passport.deserializeUser(function(googleId, done) {
+  db.Users.find({where: {google_id: googleId} }) // eslint-disable-line camelcase
+    .then(user => done(null, user))
+    .catch(err => done(err, null));
+});
+
+// configure passport to use Google OAuth2
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return db.Users.findOne({where: {google_id: profile.id} }) // eslint-disable-line camelcase
+      .then(user => {
+        if (user) { 
+          return done(null, user); 
+        } else { 
+          db.saveGoogleUser(profile)
+            .then(user => done(null, user));
+        }
+      })
+      .catch(err => done(err, null));
+  })
+);
+
+// using Google auth requires these two routes
+app.get('/auth/google',
+  passport.authenticate('google', { 
+    scope: ['https://www.googleapis.com/auth/plus.login'] 
+  })
+);
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { 
+    successRedirect: '/',
+    failureRedirect: '/' 
+  })
+);
+//////////////////////////////////
+
+
 app.get('/home', (req, res) => {
   let sqlPosts = []; // need this external variable!!
+  let user = {};
   db.searchFrontPosts() // get posts from sql for the home page
     .then(results => {
       sqlPosts = results; // set external variable
@@ -24,7 +85,9 @@ app.get('/home', (req, res) => {
     .then(results => {
       let mongoTexts = {}; // transform results into easier-to-use form
       results.forEach(record => mongoTexts[record._id] = record.text);
-      res.send(db.addMongoTextsToSqlResults(sqlPosts, mongoTexts));
+      // If user is logged in, send the user info back to the client
+      if (req.isAuthenticated()) { user = req.user; }
+      res.send({user: user, posts: db.addMongoTextsToSqlResults(sqlPosts, mongoTexts)});
     })
     .catch(err => console.log('GET /home error:', err));
 });
@@ -57,6 +120,7 @@ app.get('/search', (req, res) => {
 
 app.post('/posts', (req, res) => {
   console.log('req.body is: ', req.body);
+  console.log('req.user is:', req.user);
   let newPost = {
     // find or create user id below...
     id_users: 99999, // eslint-disable-line camelcase
@@ -67,7 +131,14 @@ app.post('/posts', (req, res) => {
     // create mongo text id below...
     id_mongo_text: '' // eslint-disable-line camelcase
   };
-  db.Users.findOrCreate({where: {username: req.body.author}})
+  db.Users.findOrCreate({
+    where: {id: req.user.id},
+    defaults: {
+      google_name: req.user.google_name, // eslint-disable-line camelcase
+      google_id: req.user.google_id, // eslint-disable-line camelcase
+      google_avatar: req.user.google_avatar // eslint-disable-line camelcase
+    }
+  })
     .spread((user, created) => {
       console.log('user from db: ', user);
       newPost.id_users = user.get('id'); // eslint-disable-line camelcase
@@ -87,6 +158,17 @@ app.post('/posts', (req, res) => {
       newPost.id_mongo_text = savedText['_id'].toString(); // eslint-disable-line camelcase
       db.Posts.create(newPost);
     });
+});
+
+// logout route added to work with passport
+app.get('/signout', function (req, res) {
+  req.logout();
+  res.clearCookie('connect.sid');
+  res.redirect('/');
+  // req.session.destroy(function (err) {
+  //   
+  //   res.redirect('/home'); 
+  // });
 });
 
 app.listen(process.env.PORT, () => {
